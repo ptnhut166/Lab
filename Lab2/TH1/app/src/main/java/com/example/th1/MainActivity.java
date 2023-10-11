ac5f62e729de1c6a8fe1097c96a48634535bfb87
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-12 00:25:37 +0700

    Initial commit

---
+++ Lab2/TH1/app/src/main/java/com/example/th1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.th1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import java.util.ArrayList;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Khoi tao ListView
+        ListView lvPerson = findViewById(R.id.lv_person);
+        final String arr[] = {"Teo", "Ty", "Bin", "Bo"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, arr);
+        lvPerson.setAdapter(adapter);
+        lvPerson.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    
+                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                        Source (arr)
+                        tvSelection.setText("position :" + arg2 + "
+                        ; value =" + arr[arg2]);
+                    }
+                });
+    }
+}
