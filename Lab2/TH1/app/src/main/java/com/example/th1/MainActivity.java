d176d145caebcabcc9c50e97621f45863fc0370f
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-13 16:13:46 +0700

    changes

--- Lab2/TH1/app/src/main/java/com/example/th1/MainActivity.java
+++ Lab2/TH1/app/src/main/java/com/example/th1/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Khoi tao ListView
+        TextView textView=findViewById(R.id.tv_person);
         ListView lvPerson = findViewById(R.id.lv_person);
         final String arr[] = {"Teo", "Ty", "Bin", "Bo"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, arr);
@@ -26,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
         lvPerson.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     
-                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                        Source (arr)
-                        tvSelection.setText("position :" + arg2 + "
-                        ; value =" + arr[arg2]);
+                    public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+
+                        textView.setText("position:"+position+";value="+arr[position]);
+                        arr[position];
+                        Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_LONG).show();
                     }
                 });
+
     }
 }
